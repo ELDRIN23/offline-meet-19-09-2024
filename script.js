b945258c8482585725a667e76b7ea1203605c662
eldrin23 <eldrinjohnsom230@gmail.com>
2024-10-19 22:02:39 +0530

    just three files

---
+++ script.js
@@ -0,0 +1,106 @@
+// let sum = 0;
+// for(let j = 1 ; j <=10 ; j++){
+   
+//     sum+=1;
+
+
+// };
+// console.log(sum + 'sum');
+// for( j = 1 ; j <= 0 ; i++){
+
+//      if(j % 2 === 0){
+//     console.log(j);
+//      };
+
+// };
+
+// let  num = 5;
+// for(let j = 1 ; j <= num ; j++){
+
+//     console.log("*".repeat(num))
+
+// }
+// output:
+// *****
+// *****
+// *****
+// *****
+// *****
+
+// function string(){
+// let patt = 5;   
+// for(let i = 0 ; i < patt ; i++){
+
+//     console.log("* ".repeat(patt));
+
+//  }
+
+// }
+
+// string();
+
+
+// let count = 5;
+// for(i = 1 ; i <= count ; i++){
+//     var pattern = ""
+//     for(j = 0 ; j <= count , j++){
+//         if(i=0){
+
+//         }
+//     }
+// }
+
+// let num = 5;
+// for(let j  = 0 ; j <= num ; j++ ){
+//     console.log("* ".repeat(j))
+    
+// }
+//0utput:
+// *
+// * *
+// * * *
+// * * * *
+// * * * * *
+
+// let num = 5;
+
+// for(let j  = 0 ; j <= num ; j++ ){
+//     console.log("* ".repeat(j))
+// }
+
+// function greet(){
+//     console.log("hello")
+// };
+// greet();
+ 
+// function vote(){
+// let age = 18;
+
+// if(age >= 18){
+//     console.log("eligible")
+// }else{
+//     console.log("not eligible")
+// };
+// return 20
+// };
+// console.log(vote(18));
+
+// function sum(a,b){
+
+//      return (b)=>{
+//         console.log(a+b)
+
+
+
+//     }
+//     return a+b;
+
+// }
+
+// sum(4,6);
+
+
+
+let arr = [1,2,3,4,5];
+arr.push(6);
+console.log(arr);
